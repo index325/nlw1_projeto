4edb10de0debe23c4499abbf5f347aff57d3b7b2
index325 <gabriel.moraes.o@hotmail.com>
2020-06-03 01:39:30 -0300

    Inserir ponto de coleta e relação com os itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,56 @@ routes.get("/items", async (request: any, response: any) => {
 
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return response.json(serializedItems);
+});
+
+routes.post("/points", async (request: any, response: any) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  try {
+    const point = await knex("points")
+      .insert({
+        image: "image-fake",
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+      })
+      .returning(["id"]);
+
+    const point_id = point[0].id;
+
+    const pointItems = items.map((item_id: number) => {
+      return {
+        item_id,
+        point_id,
+      };
+    });
+
+    await knex("point_items").insert(pointItems);
+
+    return response.json({ success: true });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 export default routes;
