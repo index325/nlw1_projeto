9755c2a07988e51808fcffee635c17e997b33f6f
index325 <gabriel.moraes.o@hotmail.com>
2020-06-03 00:57:56 -0300

    performance e rota de itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request: any, response: any) => {
-  return response.json({ message: "Hello World" });
+routes.get("/items", async (request: any, response: any) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
